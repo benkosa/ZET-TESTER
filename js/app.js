b31a87173910b21da3fcf61157275781591025bc
benkosa <benkosa0@gmail.com>
2019-11-08 19:25:54 +0100

    dokoncena logika appky a upravy v json

--- js/app.js
+++ js/app.js
@@ -7,34 +7,80 @@ var app = angular
     .controller("mainController", function ($scope, $http) {
         $http.get("data/fullData.json").then(function(response) {                       
             var data = response.data;
+            console.log(data);            
+            $scope.Data = data;
 
-            console.log(data);
+            /**
+             * generuje random int od @param min do @param max 
+             */
+            function getRandomInt(min, max) {
+                min = Math.ceil(min);
+                max = Math.floor(max);
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
 
-            data.forEach(function (test) {
+            /**
+             * pomiesa odpovede metodou ze zoberie nahodne dve odpovede
+             * a prehodi ich medzi sebou
+             * @param {treba si pozriet strukturu dat vo fullData.json} question 
+             */
+            function shuffleAnswers(question){
+                id1 = getRandomInt(0,3);
+                id2 = getRandomInt(0,3);
+                tmp = question.answers[id1];
+
+                question.answers[id1] = question.answers[id2];
+                question.answers[id2] = tmp;
+            }
+
+            /**
+             * volam pri stalceni na odpoved
+             * @param {treba si pozriet strukturu dat vo fullData.json}
+             */
+            $scope.isRight = function(answer, test, question) {
+                if(answer.right == true){
+                    //ak uz nebola zodpovedana pripocitam body
+                    if(question.answered == false){
+                        test.points++;
+                    }    
+                    answer.color = {'background-color': 'green'};
+                }else{
+                    answer.color = {'background-color': 'red'};
+                }
+                //nastavim ze uz otazka bola zodpovedana
+                //ochrana aby body pripocitavalo len ked trafim
+                //odpoved na prvy krat
+                question.answered = true;
+            };
+
+            /**
+             * vynuluje body a odpovede pre dany test a premiesa odpovede
+             * @param {treba si pozriet strukturu dat vo fullData.json} test 
+             */
+            $scope.clearTest = function(test){
+                test.points = 0;
                 test.questions.forEach(function (question) {
+                    question.answered = false;
+                    question.answers.forEach(function (answer) {
+                        answer.color = {'background-color': 'white'};
+                    });
+
+                    //pre vacsiu  nahodnost premiesavam tri krat
+                    for(var i = 0; i < 3; i++)
+                        shuffleAnswers(question);
+                });
+            }
+        });
+    })
+
+    /**
+            data.forEach(function (test) {
+                test.points = 0;
+                test.questions.forEach(function (question) {
+                    question.answered = false;
                     question.answers.forEach(function (answer) {
                         answer.color = {'background-color': 'white'};
                     });
                 });
             });
-            
-            $scope.Data = data;
-
-            $scope.isRight = function(answer) {
-                console.log(answer.right);
-                ahoj = answer.right;
-                console.log(ahoj);
-                if(answer.right == true){
-                    console.log("bol som aj tu");
-                    answer.color = {'background-color': 'green'};
-                }if(answer.right == false){
-                    console.log("bol som tu");
-                    answer.color = {'background-color': 'red'};
-                }
-                console.log(answer.right);
-              };
-
-
-
-        });
-    })
+     */
