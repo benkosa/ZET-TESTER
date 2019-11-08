669b7c5f195b9a5c4a325d0065ca1a243b0a0700
benkosa <benkosa0@gmail.com>
2019-11-08 13:19:54 +0100

    data usporiadane do jedneho velkeho json

--- js/app.js
+++ js/app.js
@@ -16,21 +16,54 @@ var app = angular
         });
         $http.get("data/tests.json").then(function(response) {                       
             Tests = response.data;
+
+            //console.log(Answers["1a"])
+
             
-            /*
             Tests.forEach(function (test) {
-                console.log(test);
-            });*/
-            for (let label in Key){
-                var intLabel = parseInt(label);
-                console.log(intLabel);
-                console.log(Key[intLabel]);
-            }
-            //console.log(Key);
-            /*
-            Key.forEach(function (test) {
-                console.log(test);
-            });*/
+                //console.log(test.name);
+                test.questions = []
+                for(var i = test.from; i <= test.to; i++){
+                    //console.log(i);
+                    question = {};
+                    question.question = Questions[i.toString()]; 
+
+                    question.answers = [];
+                    
+                    right = Key[i.toString()];
+                    
+                    switch(right){
+                        case "a":
+                                question.answers.push({"answer":Answers[i + "a"], "right":"true"});
+                                question.answers.push({"answer":Answers[i + "b"], "right":"false"});
+                                question.answers.push({"answer":Answers[i + "c"], "right":"false"});
+                                question.answers.push({"answer":Answers[i + "d"], "right":"false"});
+                        break;
+                        case "b":
+                                question.answers.push({"answer":Answers[i + "a"], "right":"false"});
+                                question.answers.push({"answer":Answers[i + "b"], "right":"true"});
+                                question.answers.push({"answer":Answers[i + "c"], "right":"false"});
+                                question.answers.push({"answer":Answers[i + "d"], "right":"false"});
+                        break;
+                        case "c":
+                                question.answers.push({"answer":Answers[i + "a"], "right":"false"});
+                                question.answers.push({"answer":Answers[i + "b"], "right":"false"});
+                                question.answers.push({"answer":Answers[i + "c"], "right":"true"});
+                                question.answers.push({"answer":Answers[i + "d"], "right":"false"});
+                        break;
+                        case "d":
+                                question.answers.push({"answer":Answers[i + "a"], "right":"false"});
+                                question.answers.push({"answer":Answers[i + "b"], "right":"false"});
+                                question.answers.push({"answer":Answers[i + "c"], "right":"false"});
+                                question.answers.push({"answer":Answers[i + "d"], "right":"true"});
+                        break;
+
+                    }
+                    test.questions.push(question);
+
+                }
+            });
+            console.log(Tests);
 
             $scope.Data = Tests;
         });
