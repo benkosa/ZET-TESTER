4dda76c517bcfff11e41518966c5cda202770bee
benkosa <benkosa0@gmail.com>
2019-11-08 10:11:25 +0100

    upravy

--- js/app.js
+++ js/app.js
@@ -1,13 +1,37 @@
+var Questions;
+var Answers;
+var Key;
+
 var app = angular
     .module("Module", [])    
     .controller("mainController", function ($scope, $http) {
         $http.get("data/questions.json").then(function(response) {                       
-            $scope.questions = response.data;
-        })
+            Questions = response.data;
+        });
         $http.get("data/answers.json").then(function(response) {                       
-            $scope.answers = response.data;
-        })
+            Answers = response.data;
+        });
         $http.get("data/key.json").then(function(response) {                       
-            $scope.key = response.data;
-        })       
+            Key = response.data;
+        });
+        $http.get("data/tests.json").then(function(response) {                       
+            Tests = response.data;
+            
+            /*
+            Tests.forEach(function (test) {
+                console.log(test);
+            });*/
+            for (let label in Key){
+                var intLabel = parseInt(label);
+                console.log(intLabel);
+                console.log(Key[intLabel]);
+            }
+            //console.log(Key);
+            /*
+            Key.forEach(function (test) {
+                console.log(test);
+            });*/
+
+            $scope.Data = Tests;
+        });
     })
